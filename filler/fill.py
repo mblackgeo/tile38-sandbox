3eef0e64bb5f9c261ca95dd6b2b90a97402567df
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-09-06 16:14:38 +0100

    Add a dockerfile for filling tile38 with real data

---
+++ filler/fill.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import geopandas as gpd
+
+
+def main():
+    """Read any GeoJSON files present in /data and put them in tile38"""
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
