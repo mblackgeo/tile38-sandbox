3e37546de200cbe589366bf0085bfddd30070649
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-09-07 14:46:57 +0100

    Update example so output can be piped to jq

--- examples/example.py
+++ examples/example.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 from pyle38 import Tile38
 from shapely import wkt
@@ -8,9 +9,9 @@ from shapely.geometry import mapping
 async def main():
     tile38 = Tile38(url="redis://localhost:9851", follower_url="redis://localhost:9851")
 
-
-    print("querying tile38 - linestring")
-    broadway = wkt.loads("""
+    # Querying a small Polygon
+    broadway = wkt.loads(
+        """
         Polygon ((
             -73.98847105508848188 40.7482867876015078,
             -73.98789783821904109 40.7481299712272147,
@@ -20,15 +21,10 @@ async def main():
         ))"""
     )
 
-    response = await tile38\
-        .within("ps")\
-        .limit(1_000_000)\
-        .object(mapping(broadway))\
-        .asObjects()
-
-    print(response.dict())
-    print("done")
-
+    response = await tile38.within("ps").limit(1_000_000).object(mapping(broadway)).asObjects()
+    print(json.dumps(response.dict()))
     await tile38.quit()
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    asyncio.run(main())
