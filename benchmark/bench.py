0ba24e28ab8944450409806eb6644e678b5c61b8
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-09-07 15:07:05 +0100

    Implement first benchmark

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -1,1 +1,25 @@
-# TODO script to benchmark calls to tile38
+import pytest
+from pyle38 import Tile38
+from shapely import wkt
+from shapely.geometry import mapping
+
+
+@pytest.mark.asyncio
+async def small_polygon_intersect(tile38):
+
+    ply = wkt.loads(
+        """
+        Polygon ((
+            -73.98847105508848188 40.7482867876015078,
+            -73.98789783821904109 40.7481299712272147,
+            -73.988996503885474 40.74299101466307604,
+            -73.9893149577018221 40.74328054389826548,
+            -73.98847105508848188 40.7482867876015078
+        ))"""
+    )
+    await tile38.within("ps").limit(1_000_000).object(mapping(ply)).asObjects()
+    await tile38.quit()
+
+
+def test_small_polygon(aio_benchmark, tile38):
+    aio_benchmark(lambda: small_polygon_intersect(tile38))
--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -4,16 +4,26 @@
 import asyncio
 
 import pytest_asyncio
+from pyle38 import Tile38
 
 
 @pytest_asyncio.fixture
 async def aio_benchmark(benchmark, event_loop):
     def _wrapper(func, *args, **kwargs):
         if asyncio.iscoroutinefunction(func):
+
             @benchmark
             def _():
                 return event_loop.run_until_complete(func(*args, **kwargs))
+
         else:
             benchmark(func, *args, **kwargs)
 
     return _wrapper
+
+
+@pytest_asyncio.fixture(scope="module", autouse=True)
+def tile38():
+    conn = Tile38(url="redis://localhost:9851", follower_url="redis://localhost:9851")
+    yield conn
+    conn.quit()
